06f965784f4d54e9823b8556000a2bfaace952ab
cody laeder <codylaeder@gmail.com>
2019-02-19 23:37:01 -0800

    well it works, improves can be made another day

--- src/lib.rs
+++ src/lib.rs
@@ -50,19 +50,15 @@ macro_rules! returnf {
 
 #[macro_export]
 macro_rules! exitf {
-    ($do: expr; CODE: $code: expr; $($arg: expr),*) => {
+    ($do: expr, $($arg:expr),*) => {
         {
             match errorf!($do, $($arg),*) {
                 Ok(x) => x,
                 Err(ref e) => {
-                    print!("{}", e);
-                    ::std::process::exit($code)
+                    panic!("{}", e);
                 }
             }
         }
-    };
-    ($do: expr, $($arg:expr),*) => {
-        exitf!($do; CODE: 1; $($arg),*)
     };
 }
 
@@ -110,78 +106,7 @@ macro_rules! errorf {
                     }
                     error_msg.push_str(&msg);
                     error_msg.push_str(EOL);
-                       
-                    // are we grabbing code?
-                    match option_env!("ERRORS_CRATE_CODE") {
-                        Option::Some(arg) => {
-                            // figure out how much we take on either side
-                            let size: u32 = match u32::from_str_radix(arg, 10) {
-                                Ok(size) => size,
-                                Err(e) => panic!("invalid value for `ERRORS_CRATE_CODE_SIZE`. value:({}) couldnt parse to number. Error:({:?})", arg, e)
-                            };
-                            error_msg.push_str("CODE: ");
-                            error_msg.push_str(concat!(file!(), ":", column!(), ":", line!()));
-                            error_msg.push_str(EOL);
-
-                            // load the local file
-                            let file: &'static str = include_str!(concat!("../", file!()));
-
-                            // safely create offsets
-                            let upper = match line!().checked_add(size) {
-                                Option::None => ::std::usize::MAX,
-                                Option::Some(upper) => upper as usize
-                            };
-                            let lower = match line!().checked_sub(size) {
-                                Option::None => 0usize,
-                                Option::Some(lower) => lower as usize,
-                            };
-
-                            // collect line numbers
-                            let line_nums: Vec<String> = Range{ start: lower, end: upper + 1}
-                                .map(|val| val.to_string())
-                                .collect();
-                            match line_nums.len() {
-                                0|1 => Err(err_msg),
-                                _ => {
-                                    let first = line_nums[0].len();
-                                    let last = line_nums[ line_nums.len() - 1 ].len();
-
-                                    // padd line numbers if needed
-                                    let bump = first != last;
-                                    let line_nums = line_nums.into_iter().map(|mut line| -> String {
-                                        loop {
-                                            if line.len() < last.len() {
-                                                line.push(' ');
-                                            } else {
-                                                break
-                                            }
-                                        }
-                                        line
-                                    }).collect::<Vec<String>>();
-
-                                    // just takes the parts of the file we need
-                                    Err(file!().lines().enumerate()
-                                        .filter_map(|(line_num,line)| -> Option<&'static str> {
-                                            if line_num.ge(&lower) && line_num.le(&upper) {
-                                                Some(line)
-                                            } else {
-                                                None
-                                            }
-                                        })
-                                        .zip(line_nums.into_iter())
-                                        .map(|(line, mut line_num)| -> String {
-                                            line_num.push_str(line);
-                                            line_num
-                                        })
-                                        .fold(error_msg, |mut buf, line| -> String {
-                                            buf.push_str(&line);
-                                            buf
-                                        }))
-                                }
-                            }
-                        },
-                        Option::Some(_) => Err(error_msg)
-                    }
+                    Err(error_msg)
                 }
             }
         }
